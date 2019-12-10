d26acddb087f74d387ee55e0849ec7d4a84833bb
atulnagpal1991 <atulnagpal1991@gmail.com>
2019-12-11 02:25:28 +0530

    add filters

--- src/Employee.js
+++ src/Employee.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class Employee extends React.Component {
   render() {
+    console.log("eee" , this.props.employees);
     return (
-    	<div className="employee-details">
-	    	<img src=""/>
-	    	<div className="employee-id">1</div>
-	    	<div className="employee-name">Stul</div>
-	    	<div className="employee-status">Single</div>
-	    	<div className="employee-species">Human</div>
-	    	<div className="employee-gender">Male</div>
-	    	<div className="employee-created">asd</div>
-	    	<div className="employee-origin">abc</div>
-	    	<div className="employee-location">Chandigah</div>
-    	</div>
+    	<>
+				{
+				    this.props.employees && this.props.employees.length > 0 && this.props.employees.map((employee, index) => {
+				        return (
+				            <div className="employee-details col-sm-6 col-lg-4 col-xl-3">
+				           	 <div className="card">
+					                <img className="card-img-top" src={employee.image}/>
+					                <div className="employee-name">{employee.name}</div>
+					                <div className="employee-id">{employee.id}</div>
+					                <div className="employee-created">{employee.created}</div>
+					             	 	<div className="employee-status"><label>Status</label><span>{employee.status}</span></div>
+					              	<div className="employee-species"><label>Species</label><span>{employee.species}</span></div>
+					             	 	<div className="employee-gender"><label>Gender</label><span>{employee.gender}</span></div>
+					             	 	<div className="employee-origin"><label>Origin</label><span>{employee.origin && employee.origin.name}</span></div>
+					             	 	<div className="employee-location"><label>Last Location</label><span>{employee.location && employee.location.name}</span></div>
+				    	    	 </div>
+				    	    	</div>
+				        )
+				    })
+				}
+    	</>
     	);
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Employee from './Employee.js';
-import './main.scss';
+import Filters from './Filters.js';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 import 'bootstrap/scss/bootstrap.scss';
+import './main.scss';
 
-class Company extends React.Component {
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+
+
+
+class Company extends Component {
+	constructor() {
+    super();
+    this.state = { employees: [] };
+  }
+
+componentDidMount() {
+  fetch("https://rickandmortyapi.com/api/character/")
+      .then(response => response.json())
+      .then((data) => {
+          console.log('Data', data)
+          this.setState({
+          	employees:data.results
+          })
+      })
+      .catch((error) => {
+          console.log('error', error)
+      })
+}
+
   render() {
     return (
-      <div class="row">
-	      <h1>Company Employee's</h1>
-	      <Employee />
+      <div className="container-fluid company-employees">
+      <Filters/>
+	      <div className="row">
+	      		<Employee employees={this.state.employees} />
+	      </div>
       </div>
     );
   }
