16417b8010ab81fcc80628d87eb8b026b5b08bee
atulnagpal1991 <atulnagpal1991@gmail.com>
2019-12-10 21:35:31 +0530

    add new file

---
+++ src/Employee.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Employee extends React.Component {
+  render() {
+    return (
+    	<div className="employee-details">
+	    	<img src=""/>
+	    	<div className="employee-id">1</div>
+	    	<div className="employee-name">Stul</div>
+	    	<div className="employee-status">Single</div>
+	    	<div className="employee-species">Human</div>
+	    	<div className="employee-gender">Male</div>
+	    	<div className="employee-created">asd</div>
+	    	<div className="employee-origin">abc</div>
+	    	<div className="employee-location">Chandigah</div>
+    	</div>
+    	);
+  }
+}
+
+export default Employee;
